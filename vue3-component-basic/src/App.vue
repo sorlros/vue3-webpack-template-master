f08f9d7247af7e8c2cbb09bf991ecc679a6f383d
sorlros <sorlros2047@gmail.com>
2022-03-31 15:54:26 +0900

    vue3-component-basic

---
+++ vue3-component-basic/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <MyBtn color="#000">Banana</MyBtn>
+  <MyBtn 
+    color="black">
+    <span style="color: red">Banana</span>
+  </MyBtn>
+  <MyBtn large color="royalblue">Apple</MyBtn>
+  <MyBtn :color="color"></MyBtn>
+  <MyBtn></MyBtn>
+  <MyBtn>Cherry</MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default { 
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-component-basic/src/components/MyBtn.vue
@@ -0,0 +1,45 @@
+<template>
+  <div 
+    class="btn"
+    :class="{ large }"
+    :style="{ backgroundColor: color }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'gray'
+    },
+    large: {
+      type: Boolean,
+      default: false
+    },
+    // text: {
+    //   type: String,
+    //   default: ''
+    // }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer; 
+    &.large {
+      font-size: 20px;
+      padding: 10px 20px;
+    }
+  }
+
+  
+</style>

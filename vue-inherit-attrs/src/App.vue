0b9b706cc870b95eb1b5ac9a58904bfcb306317d
sorlros <sorlros2047@gmail.com>
2022-03-31 16:22:38 +0900

    vue3-inherit-attrs

---
+++ vue-inherit-attrs/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <MyBtn class="sorlros" style="color: red" title="Hello World">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default { 
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-inherit-attrs/src/components/MyBtn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="btn">
+    <slot></slot>
+  </div>
+  <h1 :class="$attrs.class"
+      :style="$attrs.style">
+      
+  </h1>
+  <h1 v-bind="$attrs">
+
+  </h1>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  created() {
+    console.log(this.$attrs)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer; 
+   
+  }
+
+  
+</style>

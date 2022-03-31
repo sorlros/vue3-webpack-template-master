ea713b7eb34080ab426b64f2d2debc898d2f7e5d
sorlros <sorlros2047@gmail.com>
2022-03-31 16:48:29 +0900

    component-emit

---
+++ vue3-component-emit/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MyBtn @sorlros="log" @change-msg="logMsg">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default { 
+  components: {
+    MyBtn
+  },
+  methods: {
+    log(event) {
+      console.log('Click')
+      console.log(event)
+    },
+    logMsg(msg) {
+      console.log(msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-component-emit/src/components/MyBtn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="btn">
+    <slot></slot>
+  </div>
+  <h1 @dblclick="$emit('sorlros', $event)">ABC</h1>
+  <input type="text" v-model="msg">
+</template>
+
+<script>
+export default {
+  emits: [
+    'sorlros',
+    'changeMsg'
+  ],
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  watch: {
+    msg() {
+      this.$emit('changeMsg', this.msg)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    display: inline-block;
+    margin: 4px;
+    padding: 6px 12px;
+    border-radius: 4px;
+    background-color: gray;
+    color: white;
+    cursor: pointer; 
+  }
+
+  
+</style>

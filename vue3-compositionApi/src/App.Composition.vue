edbd864d6f7856fe021eae43e3aef40672cfae8a
sorlros <sorlros2047@gmail.com>
2022-04-02 16:32:46 +0900

    compositionApi

---
+++ vue3-compositionApi/src/App.Composition.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1
+    @click="increase">
+    {{ count }} / {{ doubleCount }}
+  </h1>
+  <h1>
+    {{ count }} / {{ reversedMessage }}
+  </h1>
+</template>
+
+<script>
+import { ref,  computed } from 'vue'
+
+export default {
+  setup() {
+    const message = ref('Hello World')
+    const reversedMessage = computed(() => {
+      return message.value.split('').reverse().join('')
+    })
+
+    const count = ref(0)
+    const doubleCount = computed(() => count.value * 2)
+    function increase() {
+      count.value += 1
+    } 
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-compositionApi/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1
+    @click="increase">
+    {{ count }} / {{ doubleCount }}
+  </h1>
+  <h1>
+    {{ count }} / {{ reversedMessage }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+      count: 0
+    }
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    },
+    reversedMessage() {
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

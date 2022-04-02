026e9f505e1462fbfbd516dc03d6269a33446892
sorlros <sorlros2047@gmail.com>
2022-04-02 15:30:37 +0900

    component-slot

---
+++ vue3-component-slot/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <MyBtn>
+    <template #icon><span>(B)</span></template>
+    <template #text><span>Banana</span></template>
+    
+    
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+export default {
+  components: {
+    MyBtn
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
